d90944911ccf4c3d557adfc7a78f7e5e0cbbc651
tummychow <tummychow@users.noreply.github.com>
2014-08-27 22:56:17 -0400

    Switch to gocheck
    I can find some good reasons to use an external testing library (less
    boilerplate and custom assertion code, easier pretty printing). I chose
    check because it's already on gopkg.in and I trust @niemeyer. It doesn't
    need me to change the way I think about tests or write them, just gives
    me some nice bonuses.

--- document/util_test.go
+++ document/util_test.go
@@ -2,8 +2,15 @@ package document_test
 
 import (
 	"github.com/tummychow/goose/document"
+	"gopkg.in/check.v1"
 	"testing"
 )
+
+func Test(t *testing.T) { check.TestingT(t) }
+
+type UtilSuite struct{}
+
+var _ = check.Suite(&UtilSuite{})
 
 var namesTable = []struct {
 	Name     string
@@ -20,38 +27,8 @@ var namesTable = []struct {
 	{"/世/界/Bar", []string{}},
 }
 
-func TestNameToSegments(t *testing.T) {
+func (s *UtilSuite) TestNameToSegments(c *check.C) {
 	for _, entry := range namesTable {
-		actual := document.NameToSegments(entry.Name)
-		equality, _ := CompareSlices(actual, entry.Segments)
-
-		if !equality {
-			t.Errorf("Name %#v produced segments %#v (expected %#v)", entry.Name, actual, entry.Segments)
-		}
+		c.Check(document.NameToSegments(entry.Name), check.DeepEquals, entry.Segments, check.Commentf("Name: %#v", entry.Name))
 	}
-}
-
-// CompareSlices compares two slices of strings for equality. The bool return
-// indicates whether the arguments are equal. The int return indicates where
-// the slices stopped being equal (if they were equal, this return value is
-// undefined). If the slices had different lengths, the int return will be -1.
-//
-// If the two slices were equal length, and all elements were pairwise equal:
-// return (true, <any integer>)
-//
-// If the two slices have unequal lengths: return (false, -1)
-//
-// If the two slices were equal length, and all elements were pairwise equal up
-// to and excluding the ith index: return (false, i)
-func CompareSlices(a, b []string) (bool, int) {
-	if len(a) != len(b) {
-		return false, -1
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false, i
-		}
-	}
-
-	return true, -1
 }
