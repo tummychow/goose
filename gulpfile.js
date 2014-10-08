8a70cc54c85722dc4d685f50b3e37bae6017ed16
tummychow <tummychow@users.noreply.github.com>
2014-10-08 15:21:03 -0400

    Create some basic gulp tasks
    A task to generate minified/concatted CSS/JS, and a task to build the
    server binary.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,26 @@
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var bower_components = require('main-bower-files')();
+
+gulp.task('css', function() {
+  return gulp.src(bower_components)
+    .pipe($.filter('**/*.css'))
+    .pipe($.concat('main.css'))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('js', function() {
+  return gulp.src(bower_components)
+    .pipe($.filter('**/*.js'))
+    .pipe($.concat('main.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('go', function() {
+  return $.run('GOPATH=$PWD/.godeps:$GOPATH go build').exec();
+});
 
 gulp.task('default', function() {
   // place code for your default task here
