4c12e40667a95a7d8ed9ad7850a399be018ea81e
tummychow <tummychow@users.noreply.github.com>
2014-10-10 19:30:00 -0400

    Refactor GOPATH calculation in gulpfile
    Moving this into a function feels cleaner than a global, since the
    calculation may be used by many tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var bower_components = require('main-bower-files')();
 
-var newenv = process.env;
-var gpm_dir = __dirname + path.sep + '.godeps';
-if (newenv.GOPATH.split(path.delimiter).indexOf(gpm_dir) == -1) {
-  newenv.GOPATH = gpm_dir + path.delimiter + newenv.GOPATH;
+var gopath = function() {
+  var gpm_dir = __dirname + path.sep + '.godeps';
+  if (process.env.GOPATH.split(path.delimiter).indexOf(gpm_dir) != -1) {
+    return process.env.GOPATH;
+  }
+  return gpm_dir + path.delimiter + process.env.GOPATH;
 }
 
 gulp.task('css', function() {
@@ -27,6 +29,8 @@ gulp.task('js', function() {
 });
 
 gulp.task('go', function() {
+  var newenv = process.env;
+  newenv.GOPATH = gopath();
   return $.run('go build', {env: newenv}).exec();
 });
 
