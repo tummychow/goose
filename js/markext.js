c33dd00f9894382e7ef34f12b4788f1a0e71b5c1
tummychow <tummychow@users.noreply.github.com>
2014-11-01 03:07:49 -0400

    Implement XSS protection
    Client-side HTML parsing will clean up any dangerous tags or dangerous
    attributes before loading the rendered markdown into the DOM.
    
    The XSS filter uses a pretty comprehensive whitelist with one special
    case: a <code class="language-foo"> element will be allowed. This is
    unavoidable because only the markdown renderer knows what language to
    specify.

--- js/markext.js
+++ js/markext.js
@@ -1,7 +1,67 @@
 marked.setOptions({langPrefix: 'language-'});
 
+var xsser = (function() {
+  var globalAttr = ['accesskey', 'dir', 'itemprop', 'lang', 'tabindex', 'title'];
+  return new filterXSS.FilterXSS({
+    whiteList: {
+      a: globalAttr + ['download', 'href', 'hreflang', 'media', 'rel', 'target'],
+      b: globalAttr,
+      blockquote: globalAttr + ['cite'],
+      br: globalAttr,
+      code: globalAttr,
+      dd: globalAttr,
+      del: globalAttr + ['cite', 'datetime'],
+      div: globalAttr + ['itemscope', 'itemtype'],
+      dl: globalAttr,
+      dt: globalAttr,
+      em: globalAttr,
+      h1: globalAttr,
+      h2: globalAttr,
+      h3: globalAttr,
+      h4: globalAttr,
+      h5: globalAttr,
+      h6: globalAttr,
+      hr: globalAttr + ['align'],
+      i: globalAttr,
+      img: globalAttr + ['align', 'alt', 'height', 'src', 'width'],
+      ins: globalAttr + ['cite', 'datetime'],
+      kbd: globalAttr,
+      li: globalAttr + ['value'],
+      ol: globalAttr + ['start'],
+      p: globalAttr,
+      pre: globalAttr,
+      q: globalAttr + ['cite'],
+      rp: globalAttr,
+      rt: globalAttr,
+      ruby: globalAttr,
+      s: globalAttr,
+      samp: globalAttr,
+      strong: globalAttr,
+      sub: globalAttr,
+      sup: globalAttr,
+      table: globalAttr + ['align'],
+      tbody: globalAttr + ['align'],
+      td: globalAttr + ['align', 'colspan', 'rowspan'],
+      tfoot: globalAttr + ['align'],
+      th: globalAttr + ['align', 'colspan', 'rowspan', 'scope'],
+      thead: globalAttr + ['align'],
+      tr: globalAttr + ['align'],
+      u: globalAttr,
+      ul: globalAttr,
+      'var': globalAttr,
+    },
+    stripIgnoreTag: true,
+    stripIgnoreTagBody: ['script', 'style'],
+    onIgnoreTagAttr: function(tag, name, value) {
+      if (tag == 'code' && name == 'class' && value.substr(0,9) == 'language-') {
+        return name + '="' + filterXSS.escapeAttrValue(value) + '"';
+      }
+    }
+  });
+})();
+
 var md = document.getElementById('md');
-md.innerHTML = marked(md.getAttribute('data-md'));
+md.innerHTML = xsser.process(marked(md.getAttribute('data-md')));
 
 (function() {
   var tables = document.querySelectorAll('table');
