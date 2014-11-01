ae747854cd84ba4de204c8d040a43e1c7634da02
tummychow <tummychow@users.noreply.github.com>
2014-11-01 02:29:42 -0400

    Move table class to postprocessing step
    We don't want to do this as part of markdown rendering, because it makes
    our XSS filter more complicated. In general, we do not want to allow the
    user to specify their own class attributes. So every class attribute
    generated by the renderer becomes a special case for the filter. The
    fewer of those, the better.

--- js/markext.js
+++ js/markext.js
@@ -6,21 +6,14 @@ marked.setOptions({
     }
     return Prism.highlight(code, Prism.languages[lang]);
   },
-  renderer: (function(){
-    var renderMin = new marked.Renderer();
-    renderMin.table = function(header, body) {
-      return '<table class=\'table\'>\n'
-      + '<thead>\n'
-      + header
-      + '</thead>\n'
-      + '<tbody>\n'
-      + body
-      + '</tbody>\n'
-      + '</table>\n';
-    };
-    return renderMin;
-  })()
 });
 
 var md = document.getElementById('md');
 md.innerHTML = marked(md.getAttribute('data-md'));
+
+(function() {
+  var tables = document.querySelectorAll('table');
+  for (var i = 0, tableElement ; tableElement = tables[i++] ; ) {
+    tableElement.className = 'table';
+  }
+})();
